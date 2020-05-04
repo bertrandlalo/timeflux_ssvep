4280935db8c48e05a38d77c99cb8d80a8f121acd
Raphaelle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2020-05-04 20:54:13 +0200

    Add dump node

---
+++ nodes/dump.py
@@ -0,0 +1,19 @@
+"""Simple debugging nodes"""
+import csv
+import pandas as pd
+from timeflux.core.node import Node
+from timeflux.core.exceptio
+
+class Dump(Node):
+    """Dump to CSV."""
+
+    def __init__(self, fname='/tmp/dump.csv'):
+        # self.writer = csv.writer(open(fname, 'a'))
+        self.list_chunks = []
+        self.fname = fname
+
+    def update(self):
+        if self.i.ready():
+            self.list_chunks.append(self.i.data)
+            # todo: save at the end only
+            pd.concat(self.list_chunks, axis=0).to_csv(self.fname)
